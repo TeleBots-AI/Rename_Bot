b3ef1cbe1f740427756eb44532c374299e5a0bb6
TeleBots-AI <77410540+TeleBots-AI@users.noreply.github.com>
2021-01-19 09:29:47 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -26,14 +26,13 @@ Type /help now for more details."""
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = "@TeleRenames_Bot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-   
-CURENT_PLAN_DETAILS = """Current plan details
+    CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
 Plan name: <a href='https://t.me/TeleBotInC/426'>{}</a>
 Expires on: {}"""
-USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-HELP_USER = """Hai <b><i>{}</i></b>, 
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """Hai <b><i>{}</i></b>, 
 I am Renamer bot ✍ by <a href='https://t.me/{}'>My Father 👨‍🏫</a>
     
 1. Send Me A Thumbnail.
@@ -50,20 +49,20 @@ I am Renamer bot ✍ by <a href='https://t.me/{}'>My Father 👨‍🏫</a>
 --------
 
 Support Group : @TeleBots_Chats"""
-REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
-REPLY_TO_DOC_FOR_RENAME_VIDEO = "Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
-ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
-IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
 The given file name has {num} characters.
 
 <b>Essays Not allowed in Telegram file name!</b>
 ©️ <code>@TeleRenames_Bot</code>
 Please short your file name and try again!"""
 
-About = """Hi __{}__,
+    About = """Hi __{}__,
 
 **📝 Language:** Python 3
 
