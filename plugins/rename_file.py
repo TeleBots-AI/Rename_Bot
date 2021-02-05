8ab35a96e7439c1eb5f5c9ee42a6a48d5b5bc033
TeleBots-AI <77410540+TeleBots-AI@users.noreply.github.com>
2021-02-05 19:58:26 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -46,7 +46,7 @@ async def rename_doc(bot, update):
         return
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
-        cmd, file_name, dis = update.text.split(" ", " ", 1)
+        cmd, file_name = update.text.split(" ", 1)
         if len(file_name) > 64:
             await update.reply_text(
                 Translation.IFLONG_FILE_NAME.format(
@@ -55,7 +55,7 @@ async def rename_doc(bot, update):
                 )
             )
             return
-        description = download_location + dis
+        description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
             chat_id=update.chat.id,
